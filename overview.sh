d18002e4c0823aebe3ff98b57953c0171a2348ec
Philip Munksgaard <philip@munksgaard.me>
2021-03-13 17:30:18 +0100

    Better overview.sh

--- overview.sh
+++ overview.sh
@@ -1,229 +1,42 @@
 #!/usr/bin/env bash
 
-BENCH=backprop
 
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
+set -o errexit
+set -o pipefail
 
 GPU=04
 
-# echo "GPU$GPU"
-echo untuned-opentuner
+benchit () {
+    BENCH="$1"
+    echo "=====  $BENCH  ====="
 
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
+    echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
+    echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
+    echo $(bc <<< "scale=2; $(cat results-gpu$GPU/$BENCH-opentuner.tunetime) / $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)")
 
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
+    echo
 
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
+    echo -n untuned-opentuner
+    ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
 
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
+    echo
 
-echo
+    echo -n untuned-autotuner
+    ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
 
-BENCH=heston32
+    echo
 
-echo "=====  $BENCH  ====="
+    echo -n opentuner-autotuner
+    ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
 
-# GPU=03
+    echo
+}
 
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=lavaMD
-
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=LocVolCalib
-
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=lud
-
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=nw
-
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=OptionPricing
-
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=srad
-
-echo "=====  $BENCH  ====="
-
-# GPU=03
-
-# echo "GPU$GPU"
-# ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json && ../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
-
-echo
-
-BENCH=bfast
-
-echo "=====  $BENCH  ====="
-
-GPU=04
-
-echo "Opentuner tuning time: $(cat results-gpu$GPU/$BENCH-opentuner.tunetime)"
-echo "Autotuner tuning time: $(cat results-gpu$GPU/$BENCH-autotuner.tunetime)"
-
-# echo "GPU$GPU"
-echo untuned-opentuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,opentuner}.json
-
-echo untuned-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{untuned,autotuner}.json
-
-echo opentuner-autotuner
-../futhark/tools/cmp-bench-json.py results-gpu$GPU/$BENCH-{opentuner,autotuner}.json
+if [ -z "$1" ]; then
+    for BENCH in backprop heston32 lavaMD LocVolCalib lud nw nn OptionPricing srad bfast pathfinder
+    do
+        benchit "$BENCH"
+    done
+else
+    benchit "$1"
+fi
