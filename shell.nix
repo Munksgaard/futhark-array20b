2bef42dc8fe18b35e859796b1288d0b3e5ace739
Philip Munksgaard <philip@munksgaard.me>
2021-03-15 13:35:20 +0100

    This seems to work better

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 with import <nixpkgs> {};
 
-pkgs.mkShell {
-  buildInputs = [ pkgs.python27 pkgs.python27Packages.numpy pkgs.python27Packages.matplotlib ];
-}
+(python27.buildEnv.override {
+  extraLibs = [ python27Packages.numpy pythonPackages.matplotlib ];
+  ignoreCollisions = true;
+}).env
